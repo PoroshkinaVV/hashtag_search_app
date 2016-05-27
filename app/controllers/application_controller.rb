fc1361c97aaf53faafb5da3662342ff6313b24a6
PoroshkinaVV <lerik-erik@pochta.ru>
2016-05-27 18:43:50 +0300

    First search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+  before_filter :load_tweets
+
+  def load_tweets
+#    @tweets = Twitter::REST::Client.bearer_token?
+#    @tweets = $client.user_timeline[0..4]
+    @tweets = $client.search("#ruby #ror")
+  end
 end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,11 @@
+require 'twitter'
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key = "dqN2QGKIojJWndXgZvbkHEzvx"
+  config.consumer_secret = 
+"WSx0PL1INU9YFRudG6eNUDRWSQbkG8WTXaroWZU6laGetbLgbx"
+  config.access_token = 
+"213308705-9l9FXcRlmS7MGFywdBksqiBNUnd4qdTEdgJuMbMJ"
+  config.access_token_secret = 
+"uA7wc6Z4cT2JlWJeg4By9BgcHYHWfluMfxbZ7HLVfCNHV"
+end
