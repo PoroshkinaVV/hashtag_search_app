9cc1f04bfaaec38113c6ab8731b17cebb2660b31
PoroshkinaVV <lerik-erik@pochta.ru>
2016-05-31 19:38:04 +0300

    editing events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,6 @@
 module EventsHelper
+  def user_options(selected)
+    options_for_select(User.all.map { |user| ["#{user.username}", 
+user.id] }, selected)
+  end
 end
--- app/helpers/hashtag_events_helper.rb
+++ app/helpers/hashtag_events_helper.rb
@@ -1,2 +1,9 @@
 module HashtagEventsHelper
+  def event_options(selected)
+    options_for_select(Event.all.map { |event| ["#{event.name}", event.id] }, selected)
+  end
+
+  def hashtag_options(selected)
+    options_for_select(Hashtag.all.map { |hashtag| ["#{hashtag.name}", hashtag.id] }, selected)
+  end
 end
